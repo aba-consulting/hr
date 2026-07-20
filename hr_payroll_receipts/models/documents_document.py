c5536cc683a8e286a6114178586b6a25692f86ea
juanmrossa2002-hue <juanmrossa2002@gmail.com>
2026-07-20 17:14:07 -0300

    Ajustar posicion firma: posX 0.13, posY 0.75

--- hr_payroll_receipts/models/documents_document.py
+++ hr_payroll_receipts/models/documents_document.py
@@ -102,8 +102,8 @@ class DocumentsDocument(models.Model):
                 'type_id': sign_type.id,
                 'responsible_id': signer_role.id,
                 'page': 1,
-                'posX': 0.15,  # Más a la derecha para centrar en la columna del empleado
-                'posY': 0.80,  # Un poco más arriba de la línea de "Firma del empleado"
+                'posX': 0.13,  # Más a la derecha para centrar en la columna del empleado
+                'posY': 0.75,  # Más arriba de la línea de "Firma del empleado"
                 'width': 0.30,  # Más ancho para cubrir el área de firma
                 'height': 0.05,  # Reducido para que no flote por encima de la línea
             }))
